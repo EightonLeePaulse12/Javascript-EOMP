5136f810ff4a0e7eeb0b66548ec84898a07cdfa6
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-06-09 19:27:14 +0200

    I accidentally pushed this content onto my main because I forgot to make a new branch, my bad ;-;. However, my add to cart button is working.

--- script/code.js
+++ script/code.js
@@ -25,15 +25,39 @@ addButton.forEach((data)=>{
         console.log(cart)
         let data = target.parentElement
         console.log(data)
-        console.log(data.firstChild.nextElementSibling.textContent)
-        let title = data.nextSibling.innerText
+        console.log(data.firstChild.nextElementSibling)
+        console.log(data.parentElement.firstChild.nextElementSibling)
+        let imageTag = data.parentElement.firstChild.nextElementSibling
+        let source = imageTag.getAttribute('src')
+        let test = data.firstChild.nextElementSibling
+        // console.log(test)
+        // console.log(test.nextElementSibling.nextElementSibling)
+        let price = test.nextElementSibling.nextElementSibling.innerText
+        let title = data.firstChild.nextElementSibling.innerText
+        console.log(title)
+        console.log(price)
         console.log(target.parentElement.parentElement)
         let addToDiv = addToHere.innerHTML += `
         <div class="row d-flex justify-content-between" id="irreplacableHeader">
-        <div class="col fw-bold" id="itemm">Title</div>
-        <div class="col fw-bold" id="itemm">Image</div>
-        <div class="col fw-bold" id="itemm">Price</div>
-        <div class="col fw-bold" id="itemm">Actions</div>
+        <div class="col fw-bold" id="itemm">${title}</div>
+        <div class="col fw-bold" id="itemm"><img id="smolImage" src="${source}"></img></div>
+        <div class="col fw-bold" id="itemm">${price}</div>
+        <div class="col fw-bold d-flex align-items-center justify-content-center" id="actions">
+                  <button class="btn" id="minus">-</button>
+                  <h5 id="quantity">1</h5>
+                  <button class="btn" id="add">+</button>
+                </div>
+      </div>
+      <div class="row d-flex justify-content-between">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col">
+          <h6>Total: </h6>
+        </div>
+      </div>
+      <div class="row d-flex justify-content-center">
+        <button id="Checkout" class="btn-danger">Checkout</button>
       </div>
         `
         content.push(addToDiv)
@@ -44,6 +68,8 @@ addButton.forEach((data)=>{
         buttons = []
     })
 })
+
+console.log("REACHED")
 
 // function addToTheCart(event){
 //     let target = event.target
